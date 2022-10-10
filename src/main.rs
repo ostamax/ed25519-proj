1df2d8c1b3d3b10ef214053b286f7bf4c376989d
ostamax <makostapenko@gmail.com>
2022-10-10 23:57:31 +0300

    Add formatting for main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,28 @@
 extern crate clap;
-extern crate rand;
 extern crate ed25519_dalek;
+extern crate rand;
 
 use std::fs;
 use std::io::Read;
 
 use clap::Parser;
-use ed25519_dalek::SecretKey;
-use rand::rngs::OsRng;
 use ed25519_dalek::Keypair;
+use ed25519_dalek::PublicKey;
+use ed25519_dalek::SecretKey;
 use ed25519_dalek::Signature;
 use ed25519_dalek::Signer;
 use ed25519_dalek::Verifier;
-use ed25519_dalek::PublicKey;
+use rand::rngs::OsRng;
 
-use ed25519_dalek::{PUBLIC_KEY_LENGTH, SECRET_KEY_LENGTH, KEYPAIR_LENGTH, SIGNATURE_LENGTH};
-
+use ed25519_dalek::{PUBLIC_KEY_LENGTH, SECRET_KEY_LENGTH, SIGNATURE_LENGTH};
 
 pub fn generate_keys() -> Keypair {
-    let mut csprng = OsRng{};
+    let mut csprng = OsRng {};
     let keypair: Keypair = Keypair::generate(&mut csprng);
     keypair
 }
 
-pub fn write_keys_to_file(keypair: Keypair){
+pub fn write_keys_to_file(keypair: Keypair) {
     let public_key_bytes: [u8; PUBLIC_KEY_LENGTH] = keypair.public.to_bytes();
 
     let secret_key_bytes: [u8; SECRET_KEY_LENGTH] = keypair.secret.to_bytes();
@@ -31,19 +30,19 @@ pub fn write_keys_to_file(keypair: Keypair){
     println!("{:?}", secret_key_bytes.len());
     fs::write("pk.txt", public_key_bytes).unwrap();
     fs::write("sk.txt", secret_key_bytes).unwrap();
-
 }
 
 pub fn read_message_from_file() -> String {
     let mut file = fs::File::open("message.txt").expect("Unable to open");
     let mut message = String::new();
-    file.read_to_string(&mut message).expect("Error while reading file");
-    message  
+    file.read_to_string(&mut message)
+        .expect("Error while reading file");
+    message
 }
 
 pub fn read_secret_key_from_file(secret_key_path: &str) -> SecretKey {
     let mut file = fs::File::open(secret_key_path).expect("Unable to open");
-    
+
     let mut secret_key_bytes = vec![];
     file.read_to_end(&mut secret_key_bytes).unwrap();
     println!("{:?}", secret_key_bytes);
@@ -58,13 +57,19 @@ pub fn read_public_key_from_file(public_key_path: &str) -> PublicKey {
     file.read_to_end(&mut public_key_bytes).unwrap();
     let public_k: PublicKey = PublicKey::from_bytes(&public_key_bytes).unwrap();
     public_k
-
 }
 
-pub fn sign_message(message_string: String, secret_key_path: &str, public_key_path: &str) -> Signature {
+pub fn sign_message(
+    message_string: String,
+    secret_key_path: &str,
+    public_key_path: &str,
+) -> Signature {
     let secret_key = read_secret_key_from_file(secret_key_path);
     let public_key = read_public_key_from_file(public_key_path);
-    let keypair  = Keypair{public:public_key, secret:secret_key};
+    let keypair = Keypair {
+        public: public_key,
+        secret: secret_key,
+    };
     let message: &[u8] = message_string.as_bytes();
     let signature = keypair.sign(message);
     println!("{:?}", signature);
@@ -85,22 +90,25 @@ pub fn read_signature_from_file(signature_path: &str) -> Signature {
     signature
 }
 
-pub fn verify_signature(message_string: String, signature_path: &str, public_key_path: &str) -> bool {
+pub fn verify_signature(
+    message_string: String,
+    signature_path: &str,
+    public_key_path: &str,
+) -> bool {
     let public_key = read_public_key_from_file(public_key_path);
     let signature = read_signature_from_file(signature_path);
     let message: &[u8] = message_string.as_bytes();
     public_key.verify(message, &signature).is_ok()
 }
 
-#[derive(Parser,Default,Debug)]
+#[derive(Parser, Default, Debug)]
 struct Args {
-   operation_type: String,
-   message: Option<String>,
-   key_path: Option<String>,
+    operation_type: String,
+    message: Option<String>,
+    key_path: Option<String>,
 }
 
 fn main() {
-
     let args = Args::parse();
     println!("{}", args.operation_type);
 
@@ -108,19 +116,20 @@ fn main() {
         "generate" => {
             let generated_keypair = generate_keys();
             write_keys_to_file(generated_keypair);
-        },
+        }
         "sign" => {
             println!("Signing");
             let message = read_message_from_file();
             println!("{:?}", message);
             sign_message(String::from("f"), "sk.txt", "pk.txt");
-        },
+        }
         "verify" => {
             println!("Verifying");
-            println!("{}", verify_signature(String::from("f"), "signature.txt", "pk.txt"));
-
-        },
+            println!(
+                "{}",
+                verify_signature(String::from("f"), "signature.txt", "pk.txt")
+            );
+        }
         _ => panic!("unknown operation"),
     }
-
 }
